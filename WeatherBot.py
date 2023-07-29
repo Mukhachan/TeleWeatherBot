69e8623211465183f0f644c2b7ccd53a284b2444
Mukhamet1996 <aralmuhamet@gmail.com>
2023-07-29 22:39:18 +0300

    1.0.4.1

--- WeatherBot.py
+++ WeatherBot.py
@@ -241,7 +241,7 @@ async def shedule_handler():
             people_list = db_connect_old().search_by_city_and_nowtime(city) # Получаем список пользователей которым нужно отправить погоду
             for person in people_list: # Перебираем список пользователей #
                 print(person)
-                if person['city'] not in weather_cache and (datetime.now() - weather_cache['last_update']).total_seconds()/60 > 3*60: # Если в кэше нет нужного города, то обновляем его и едем дальше
+                if person['city'] not in weather_cache or (datetime.now() - weather_cache['last_update']).total_seconds()/60 > 3*60: # Если в кэше нет нужного города, то обновляем его и едем дальше
                     cache_response = get_weather_cache()
                     cities = cache_response[0]
                     weather_cache = cache_response[1]
